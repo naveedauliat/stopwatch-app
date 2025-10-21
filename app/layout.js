d09d9dceda904c780e348ec33564f8cb0f99251e
nav <101603830+naveedauliat@users.noreply.github.com>
2025-10-21 17:11:44 +0500

    Fixed gaasd

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: {
-    default: "Online Stopwatch – Free & Simple Timer, No Ads, No Signup Needed, Just Time.",
+    default: "Online Stopwatch – Free & Simple Timer, No Ads, No Signups",
     template: "%s | Stopwatch.lol",
   },
   description: "A free, simple, and accurate online stopwatch and timer. Start, stop, and reset instantly – no downloads required. For workouts, study sessions, and more.",
