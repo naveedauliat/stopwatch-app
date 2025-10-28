87670fe9719d94bef116843f538d1e48e2770091
nav <101603830+naveedauliat@users.noreply.github.com>
2025-10-28 15:20:37 +0500

    updated sitemaps

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://stopwatch.lol', // your main domain
+  generateRobotsTxt: true,           // (optional) also generates robots.txt
+  sitemapSize: 7000,                 // split if you ever exceed 7000 pages
+  changefreq: 'daily',               // how often pages change
+  priority: 0.7,                     // default priority
+  generateIndexSitemap: true,
+}
