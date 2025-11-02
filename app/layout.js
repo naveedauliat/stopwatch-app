aad86a0e28a41f6d9560c6b546649baf36ba77f6
nav <101603830+naveedauliat@users.noreply.github.com>
2025-11-02 16:58:23 +0500

    fixed googletags

--- app/layout.js
+++ app/layout.js
@@ -79,9 +79,30 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Remove old GA scripts from head */}
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NRZK8XTX');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body className={`${inter.className} flex flex-col min-h-screen`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NRZK8XTX"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Header />
         <main className="flex-grow">
           {children}
